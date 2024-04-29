faff3f839ad8723def2a0ed15bfdd88b0b1c2457
charlesperrillat <ce.perrillat@gmail.com>
2024-04-29 21:16:06 +0200

    uploading the program on GitHub

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _1_Delegates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ShowMoney showAmount = GetAmount;
+        Console.WriteLine(showAmount(100.50) + " €");
+        Console.WriteLine(showAmount(100.50) + " $");
+    }
+
+    public delegate double ShowMoney(double amount);
+
+    public static double GetAmount(double amount)
+    {
+        return amount;
+    }
+
+}
+
